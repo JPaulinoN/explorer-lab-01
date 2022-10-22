bcae56f4b2014d12a458a0ade1a24257f72be67f
Joao Vitor <j.vitor.paulino.nobre@gmail.com>
2022-10-22 16:41:51 -0400

    commit innicial

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const ccBgColor01 = document.querySelector(".cc-bg.svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg.svg > g g:nth-child(2) path")
 const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
 
-function setCardType(){
+function setCardType(type) {
     const colors = {
         visa: ["#436D99", "#2D57F2"],
         mastercard: ["#DF6F29", "#C69347"],
