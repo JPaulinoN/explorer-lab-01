b551b60e3912f3d851af561541a62114976c1bd0
Joao Vitor <j.vitor.paulino.nobre@gmail.com>
2022-10-23 21:24:33 -0400

    aula 02 pela metade lmao

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import "./css/index.css"
+import IMask from "imask"
 
-const ccBgColor01 = document.querySelector(".cc-bg.svg > g g:nth-child(1) path")
-const ccBgColor02 = document.querySelector(".cc-bg.svg > g g:nth-child(2) path")
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
 const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+console.log(ccBgColor01)
 
 function setCardType(type) {
     const colors = {
@@ -11,7 +13,7 @@ function setCardType(type) {
         rocketseat: ["#0D6F5D", "#C3129C"],
         default: ["black", "gray"],
     }
-
+    console.log(type)
     ccBgColor01.setAttribute("fill", colors[type][0])
     ccBgColor02.setAttribute("fill", colors[type][1])
     ccLogo.setAttribute("src", `cc-${type}.svg`)
